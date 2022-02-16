e3b64f15780cf56f293c62a8013355ec306c4206
Titus Dolphus <tdolphus@bgea.org>
2022-02-16 10:32:00 -0500

    Remove console.log

---
+++ public/js/app.js
@@ -0,0 +1,29 @@
+
+
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const location = search.value;
+
+  if(!location) return messageOne.textContent = 'You must submit a location';
+
+  messageOne.textContent = 'Loading...';
+  messageTwo.textContent = '';
+
+  fetch("http://localhost:4000/weather?address=" + location).then((res) => {
+    res.json().then((data) => {
+      if (data.error) return messageOne.textContent = data.error;
+
+      
+
+      messageOne.textContent = data.location;
+      messageTwo.textContent = data.forecast;
+    });
+  });
+
+
+});
