a70a822215c699062c4135f79f0c7d84d9c362a4
Titus Dolphus <tdolphus@bgea.org>
2022-02-23 09:52:18 -0500

    setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,29 +1,23 @@
-
-
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
-const messageOne = document.querySelector('#message-1')
-const messageTwo = document.querySelector('#message-2')
+const messageOne = document.querySelector("#message-1");
+const messageTwo = document.querySelector("#message-2");
 
 weatherForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const location = search.value;
 
-  if(!location) return messageOne.textContent = 'You must submit a location';
+  if (!location) return (messageOne.textContent = "You must submit a location");
 
-  messageOne.textContent = 'Loading...';
-  messageTwo.textContent = '';
+  messageOne.textContent = "Loading...";
+  messageTwo.textContent = "";
 
-  fetch("http://localhost:4000/weather?address=" + location).then((res) => {
+  fetch("/weather?address=" + location).then((res) => {
     res.json().then((data) => {
-      if (data.error) return messageOne.textContent = data.error;
-
-      
+      if (data.error) return (messageOne.textContent = data.error);
 
       messageOne.textContent = data.location;
       messageTwo.textContent = data.forecast;
     });
   });
-
-
 });
